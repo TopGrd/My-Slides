4958aa249cb583125f4d20e79fd2813c6f46d752
topgrd <topgrd@outlook.com>
2017-06-30 09:58:56 +0800

    generator markdowns for slides

---
+++ README.md
@@ -0,0 +1,3 @@
+## Internal JavaScript Traning Slides
+
+
---
+++ js/generator.js
@@ -0,0 +1,37 @@
+/**
+ * File: e:\Session\Amazing-Javascript\js\generator.js
+ * Project: e:\Session\Amazing-Javascript
+ * Created Date: Thu Jun 29 2017
+ * Author: topgrd
+ * Email: topgrd@outlook.com
+ * -----
+ * Last Modified: Thu Jun 29 2017
+ * Modified By: topgrd
+ * -----
+ * Copyright (c) 2017
+ */
+
+
+const App = {
+  slides: document.querySelector('.slides'),
+  total: 2,
+  baseUrl: 'content/'
+}
+
+function genSections(index, { baseUrl, slides } = App) {
+  const section = document.createElement('section')
+  section.setAttribute('data-markdown', `${baseUrl}${index}.md`)
+  section.setAttribute('data-separator', '^\n\n\n')
+  section.setAttribute('data-separator-vertical', '^\n\n')
+  section.setAttribute('data-separator-notes', '^Note:')
+  section.setAttribute('data-charset', 'utf-8')
+  slides.appendChild(section)
+}
+
+App.start = function () {
+  for (let index = 1; index <= App.total; index++) {
+    genSections(index)
+  }
+}
+
+App.start()
