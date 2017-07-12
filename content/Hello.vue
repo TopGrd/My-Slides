ca47f0d751619520b405d938a63740529d64704a
topgrd <topgrd@outlook.com>
2017-07-12 15:59:06 +0800

    react vue

---
+++ content/Hello.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>{{message}} {{name}}</h2>
+    <button @click="changeName"></button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello',
+      name: 'World'
+    }
+  },
+  methods: {
+    changeName() {
+      this.name = 'Vue'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- js/generator.js
+++ js/generator.js
@@ -5,7 +5,7 @@
  * Author: topgrd
  * Email: topgrd@outlook.com
  * -----
- * Last Modified: Mon Jul 03 2017
+ * Last Modified: Wed Jul 12 2017
  * Modified By: topgrd
  * -----
  * Copyright (c) 2017
@@ -14,7 +14,7 @@
 
 const App = {
   slides: document.querySelector('.slides'),
-  total: 40,
+  total: 50,
   baseUrl: 'content/'
 }
 
